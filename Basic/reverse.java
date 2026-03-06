9afe0506f324b496b407b1bc4547f144d992c6b3
sumit yadav <sumityadav70539160@gmail.com>
2026-03-06 22:06:43 +0530

    Reverse of number

---
+++ Basic/reverse.java
@@ -0,0 +1,18 @@
+public class reverse {
+    public static void main(String args []){
+    reverse(4321);
+        System.out.println(sum);
+
+    }
+   static int sum =0;
+    static void reverse(int n){
+        if(n==0){
+            return;
+        }
+        int rem =n%10;
+        sum=sum*10+rem;
+        reverse(n/10);
+
+    }
+
+}
