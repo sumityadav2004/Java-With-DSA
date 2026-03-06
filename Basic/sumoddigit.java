c93f7d53e55c3302d6d318d9a017925d6749a8e3
sumit yadav <sumityadav70539160@gmail.com>
2026-03-06 22:01:42 +0530

    Sum of odd digit

---
+++ Basic/sumoddigit.java
@@ -0,0 +1,12 @@
+public class sumoddigit {
+    public static void main(String args []){
+         System.out.println(sum(1345));
+    }
+    static int sum(int n){
+        if(n==0){
+            return 0;
+
+        }
+        return (n%10)+sum(n/10);
+    }
+}
