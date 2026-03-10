95e40439a388bd6ba3ff4ece0b4bc1fba61032ce
sumit yadav <sumityadav70539160@gmail.com>
2026-03-10 22:31:26 +0530

    fibonacci number

---
+++ Important Question/fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class fibonacci{
+    public static void main(String args[]){
+        int n1=0;
+        int n2=1;
+        int n3;
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(n1 +""+ n2);
+        
+        for(int i=2;i<=n;i++){
+            n3=n2+n1;
+            System.out.println(" " +n3);
+            n1=n2;
+            n2=n3;
+        }
+    }
+}
