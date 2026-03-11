d3e9d7c5a76f6c932db3015ebefc7dfb1b34f960
sumit yadav <sumityadav70539160@gmail.com>
2026-03-11 22:13:22 +0530

    Important concept

--- Important Question/swaping.java
+++ Important Question/swaping.java
@@ -3,17 +3,18 @@ public class swaping {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
         int temp;
-        int a=sc.nextInt();
         System.out.println("enter the number a:");
-        int b=sc.nextInt();
+        int a=sc.nextInt();
         System.out.println("enter the number b:");
+        int b=sc.nextInt();
+       
          
         temp=a;
         a=b;
         b=temp;
-
+        System.out.println("Swapping value is");
         System.out.println(a);
         System.out.println(b);
-        
+ 
     }
 }
