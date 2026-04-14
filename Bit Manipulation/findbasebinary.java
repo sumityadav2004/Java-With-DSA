2325b271db86245a2a879887a8a1b6f181c9795e
sumit yadav <sumityadav70539160@gmail.com>
2026-04-14 22:38:10 +0530

    Bit Manipulation

---
+++ Bit Manipulation/findbasebinary.java
@@ -0,0 +1,10 @@
+public class findbasebinary {
+    public static void main(String args []){
+        int n =78;
+        int base = 2;
+
+        int ans = (int) (Math.log(n) / Math.log(base))+1;
+     System.out.println(ans);
+
+    }
+}
